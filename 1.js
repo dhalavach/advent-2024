393d2f3c41663240df2f6c0a304907dc9b079017
Dzmitry Halavach <kotik1@protonmail.com>
2024-12-02 14:20:15 +0300

    init: 1st puzzle

---
+++ 1.js
@@ -0,0 +1,29 @@
+import fs from 'fs/promises';
+
+async function solve(pathToInput) {
+  const data = await fs.readFile(pathToInput, 'utf-8');
+
+  const regex = /(\d+)\s+(\d+)/g;
+
+  const firstNumbers = [];
+  const secondNumbers = [];
+  let match;
+
+  while ((match = regex.exec(data)) !== null) {
+    firstNumbers.push(parseInt(match[1], 10));
+    secondNumbers.push(parseInt(match[2], 10));
+  }
+
+  firstNumbers.sort((a, b) => a - b);
+  secondNumbers.sort((a, b) => a - b);
+
+  const result = firstNumbers.reduce((acc, curr, i) => {
+    return firstNumbers[i] >= secondNumbers[i]
+      ? acc + firstNumbers[i] - secondNumbers[i]
+      : acc + secondNumbers[i] - firstNumbers[i];
+  }, 0);
+
+  console.log(result);
+}
+
+solve('input-1.txt');
